1cb9bc3a71a518fe41c2429b0714b9e06fdd6a49
Matthew Redlinger <mredlinger@wisc.edu>
2023-04-13 20:57:46 -0500

    updated accordion but no info yet

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function updateNavLinks() {
-    let urlParts = window.location.href.split('/');
-    let pageName = urlParts[urlParts.length - 1].replace('.html', '');
-    $('#' + pageName).addClass('active');
+  let urlParts = window.location.href.split("/");
+  let pageName = urlParts[urlParts.length - 1].replace(".html", "");
+  $("#" + pageName).addClass("active");
 }
 
-$( document ).ready(function() {
-    updateNavLinks();
+$(document).ready(function () {
+  updateNavLinks();
 });
+
+function myFunc(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
