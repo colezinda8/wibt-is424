4552052e7e4d365effe69731488f85fb777cb9ea
colezinda8 <126021686+colezinda8@users.noreply.github.com>
2023-04-17 21:37:18 -0500

    Updated Styling

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function updateNavLinks() {
 }
 
 $(document).ready(function () {
+  SlideShow(slidePosition);
   updateNavLinks();
 });
 
@@ -15,4 +16,28 @@ function myFunc(id) {
   } else {
     x.className = x.className.replace(" w3-show", "");
   }
+}
+
+// Slideshow functions 
+var slidePosition = 1;
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slidePosition-1].style.display = "flex";
 }
