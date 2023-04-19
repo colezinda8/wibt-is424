a74be61f1eab32cbe0caa88a417ae57261a2ab27
colezinda8 <126021686+colezinda8@users.noreply.github.com>
2023-04-18 21:42:38 -0500

    Styling updates & preparing for deploy

---
+++ WIBT/calendar.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtCVG7aHc8mqYUa61X3MejlUYHQSYApgM",
+  authDomain: "wibt-60aff.firebaseapp.com",
+  projectId: "wibt-60aff",
+  storageBucket: "wibt-60aff.appspot.com",
+  messagingSenderId: "157489052248",
+  appId: "1:157489052248:web:e2c63c229bec07d174e82d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
