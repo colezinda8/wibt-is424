02a2ad3a3f4509f28b0940e4e49e57d0394ae6cc
colezinda8 <126021686+colezinda8@users.noreply.github.com>
2023-04-25 00:17:33 -0500

    Made it responsive for phones
    Will still look terrible on an ipad

--- WIBT/scripts/script.js
+++ WIBT/scripts/script.js
@@ -25,6 +25,15 @@ function updateNavLinks() {
 
 $(document).ready(function () {
   updateNavLinks();
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+  });
 });
 
 function myFunc(id) {
