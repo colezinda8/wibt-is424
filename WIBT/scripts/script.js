5927a18655e88f2a58652400ffc6675e3fecb7cd
Leo-Curatola <lcuratola@wisc.edu>
2023-04-27 12:16:17 -0500

    cleaned up merge

--- WIBT/scripts/script.js
+++ WIBT/scripts/script.js
@@ -91,76 +91,7 @@ function UserCheck() {
     }
 };
 
-var UC = 0;
-r_e("index").onload = UserCheck();
 
-function r_e(id) {
-    return document.querySelector(`#${id}`);
-}
-
-login_submit.addEventListener("click", () => {
-    signin_modal.classList.remove("is-active");
-    eventbtn.classList.remove("is-hidden");
-    r_e("signin_form").reset();
-    console.log("hello");
-    r_e("signinbtn").innerHTML = `Log Out`;
-    UC = 1;
-    console.log(UC);
-});
-
-let signinbtn = document.querySelector("#signinbtn");
-let signin_modal = document.querySelector("#signin_modal");
-let signin_modalbg = document.querySelector("#signin_modalbg");
-
-login_submit.addEventListener("click", (e) => {
-    e.preventDefault();
-    //alert('test');
-
-    //    let email = r_e("email_").value;
-    //    let password = r_e("password_").value;
-    //    firebase.auth().createUserWithEmailAndPassword(email, password).then((user) => {
-
-    //console.log(`${user.user.email}  is created`);
-    //        console.log(user);
-    //    })
-    //.catch(err => {
-    //signup_modal.querySelector('.error').innerHTML = err.message;
-
-    // })
-    signin_modal.classList.remove("is-active");
-    //console.log("hello");
-    r_e("signinbtn").innerHTML = `Log Out`;
-    //r_e('user_add').innerHTML = `Add User`;
-    UC = 1;
-    r_e("signin_form").reset();
-});
-
-function UserCheck() {
-    if (UC == 0) {
-        r_e("signinbtn").innerHTML = `Admin Portal`;
-        user_add_div.classList.add("is-hidden");
-    } else if (UC == 1) {
-        r_e("signinbtn").innerHTML = `Log Out`;
-        user_add_div.classList.remove("is-hidden");
-    }
-}
-
-
-//let email = r_e("email_").value;
-//let password = r_e("password_").value;
-//auth.signInWithEmailAndPassword(email, password).then((user) => {
-
-//console.log(`${user.user.email}  is created`);
-
-//configure_message_bar(`${user.user.email}  is signed in`);
-//r_e('signin_form').reset();
-//r_e('signin_modal').classList.remove('is-active');
-
-//}).catch(err => {
-//signin_modal.querySelector('.error').innerHTML = err.message;
-
-//r_e('signup_form').requestFullscreen();
-//or
 
 signinbtn.addEventListener("click", () => {
     if (r_e("signinbtn").innerHTML == "Admin Portal") {
