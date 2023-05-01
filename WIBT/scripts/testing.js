489638cd1c07e72926c883fec961359db95da857
Camden Flater <crflater@wisc.edu>
2023-05-01 18:41:40 -0500

    Created automation

--- WIBT/scripts/testing.js
+++ WIBT/scripts/testing.js
@@ -5,14 +5,16 @@ const puppeteer = require("puppeteer");
 async function go() {
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 15,
+    slowMo: 350,
   });
 
   const page = await browser.newPage();
 
+  await page.setViewport({ width: 1000, height: 800 });
+
   //   site the to be tested
 
-  await page.goto("http://127.0.0.1:5500/WIBT/");
+  await page.goto("http:wibt-uwmadison.org");
 
   //   user click the sign-in button
 
@@ -20,16 +22,32 @@ async function go() {
 
   // user will provide email, password, and interests information
 
-  await page.type("#email_", "test@test.com");
-  await page.type("#password_", "1234");
-
+  await page.type("#email_", "a@a.com");
+  await page.type("#password_", "aaaaaa");
   await page.click("#login_submit");
 
-  //   test the search functionality
+  // add event
+  await page.click("#homeEvents > div.event.event-add");
+  await page.type("#title", "Test Event");
+  await page.type("#date", "5/5/2023");
+  await page.click("#title");
+  await page.type("#hour", "2");
+  await page.type("#minute", "30");
+  await page.click("#event_submit");
+  //  delete event
+  await page.evaluate(() => {
+    let elements = document.getElementsByClassName(
+      "button is-small is-rounded delete-button m-0"
+    );
+    elements[0].click();
+  });
+  await page.click("#event_delete");
+  //  sign out
+  await page.click("#signinbtn");
+  await page.click("#login_submit");
 
-  //   set 2 second delay
-
-  await new Promise((r) => setTimeout(r, 2000));
+  // close
+  browser.close();
 }
 
 // call go()
