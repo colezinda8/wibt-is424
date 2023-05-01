1f2ed48be9e503ffc0e03088ddc771e207f39dae
colezinda8 <126021686+colezinda8@users.noreply.github.com>
2023-05-01 11:13:09 -0500

    Site Finalized

--- WIBT/scripts/script.js
+++ WIBT/scripts/script.js
@@ -52,7 +52,6 @@ function r_e(id) {
 let signinbtn = document.querySelector("#signinbtn");
 let signin_modal = document.querySelector("#signin_modal");
 let signin_modalbg = document.querySelector("#signin_modalbg");
-let eventbtn = document.querySelector("#eventbtn");
 let event_modal = document.querySelector("#event_modal");
 
 login_submit.addEventListener("click", (e) => {
@@ -72,7 +71,6 @@ login_submit.addEventListener("click", (e) => {
     signin_modal.classList.remove("is-active");
     //console.log("hello");
     r_e("signinbtn").innerHTML = `Log Out`;
-    eventbtn.classList.add("is-active");
     //r_e('user_add').innerHTML = `Add User`;
     UC = 1;
     r_e("signin_form").reset();
@@ -84,7 +82,6 @@ function UserCheck() {
         //        user_add_div.classList.add('is-hidden');
     } else if (UC == 1) {
         r_e("signinbtn").innerHTML = `Log Out`;
-        eventbtn.classList.add("is-active");
         //      user_add_div.classList.remove('is-hidden');
     }
 }
@@ -136,7 +133,6 @@ auth.onAuthStateChanged(function (user) {
         password_bar.classList.add('is-hidden');
         r_e("login_submit").innerHTML = "Log Out";
         reloadEvents();
-        // eventbtn.classList.add("is-active"); !!!!!!!UNDO BEFORE COMMITTING!!!!!!!!!!
     } else {
         r_e("signinbtn").innerHTML = `Admin Portal`;
         user_add_div.classList.add('is-hidden');
@@ -144,7 +140,6 @@ auth.onAuthStateChanged(function (user) {
         password_bar.classList.remove('is-hidden');
         r_e("login_submit").innerHTML = "Login";
         reloadEvents();
-        // eventbtn.classList.add("is-hidden"); !!!!!!!UNDO BEFORE COMMITTING!!!!!!!!!!
     }
 });
 
@@ -192,19 +187,31 @@ if (pageName == "index.html" || pageName == "events.html") {
     document.getElementById("event_modalbg").addEventListener("click", () => {
         event_modal.classList.remove("is-active");
         r_e("event_form").reset();
+        $('#addError').text("");
     });
 
     document.getElementById("event_edit_modalbg").addEventListener("click", () => {
         $('#event_edit_modal').removeClass("is-active");
-        r_e("event_form").reset();
+        r_e("event_edit_form").reset();
+        $('#editError').text("");
     });
 
     $('#close_delete').click(function () {
         $('#deleteModal').removeClass("is-active");
+        $('#eventToDelete').attr("event_id", "");
+        $('#deleteError').text("");
+    });
+
+    $('#delete_modalbg').click(function () {
+        $('#deleteModal').removeClass("is-active");
+        $('#eventToDelete').attr("event_id", "");
+        $('#deleteError').text("");
     });
 
     $('#event_edit_close').click(function () {
         $('#event_edit_modal').removeClass("is-active");
+        r_e("event_edit_form").reset();
+        $('#editError').text("");
     });
 
     $('#event_delete').click(function () {
@@ -217,6 +224,8 @@ if (pageName == "index.html" || pageName == "events.html") {
 
     $('#close_add').click(function () {
         event_modal.classList.remove("is-active");
+        r_e("event_form").reset();
+        $('#addError').text("");
     });
 }
 
@@ -225,7 +234,15 @@ function add_doc() {
     let title = document.querySelector("#title").value;
     let date = document.querySelector("#date").value;
     let hour = document.querySelector("#hour").value;
+    if (hour.length > 2 || hour.length < 1 || parseInt(hour) > 12 || parseInt(hour) < 1) {
+        $('#addError').text("Invalid Hour Entry. Must be between 1 & 12.");
+        return;
+    }
     let minute = document.querySelector("#minute").value;
+    if (minute.length > 2 || minute.length < 1 || parseInt(minute) > 59 || parseInt(minute) < 0) {
+        $('#addError').text("Invalid Minutes Entry. Must be between 1 & 59.");
+        return;
+    }
     let ampm = document.querySelector("#ampm").value;
     let time = hour + ":" + minute + " " + ampm;
 
@@ -234,10 +251,18 @@ function add_doc() {
         Date: date,
         Time: time,
     };
-    db.collection("events").add(new_event).then(() => {
-        reloadEvents();
-        event_modal.classList.remove("is-active");
-    });
+
+    if (auth.currentUser) {
+        db.collection("events")
+            .add(new_event)
+            .catch((error) => {
+                $('#addError').text("There was an issue communicating with the DB. Please try again later.");
+            })
+            .then(() => {
+                reloadEvents();
+                event_modal.classList.remove("is-active");
+            });
+    }
 }
 
 //create editing
@@ -268,19 +293,34 @@ function submit_edit() {
     let title = document.getElementById("edit_title").value;
     let date = document.getElementById("edit_date").value;
     let hour = document.getElementById("edit_hour").value;
+    if (hour.length > 2 || hour.length < 1 || parseInt(hour) > 12 || parseInt(hour) < 1) {
+        $('#editError').text("Invalid Hour Entry. Must be between 1 & 12.");
+        return;
+    }
     let minute = document.getElementById("edit_minute").value;
+    if (minute.length > 2 || minute.length < 1 || parseInt(minute) > 59 || parseInt(minute) < 0) {
+        $('#editError').text("Invalid Minutes Entry. Must be between 1 & 59.");
+        return;
+    }
     let ampm = document.getElementById("edit_ampm").value
     let time = hour + ":" + minute + " " + ampm;
     let id = $('#eventToEdit').attr("event_id");
 
-    db.collection("events").doc(id).update({
-        Title: title,
-        Date: date,
-        Time: time
-    }).then(() => {
-        reloadEvents();
-        $('#event_edit_modal').removeClass("is-active");
-    });
+    if (auth.currentUser) {
+        db.collection("events").doc(id).update({
+            Title: title,
+            Date: date,
+            Time: time
+        })
+            .catch((error) => {
+                $('#editError').text("There was an issue communicating with the DB. Please try again later.");
+            })
+            .then(() => {
+                reloadEvents();
+                $('#event_edit_modal').removeClass("is-active");
+                $('#editError').text("");
+            });
+    }
 }
 
 function del_doc(id) {
@@ -293,6 +333,9 @@ function submit_delete() {
     db.collection("events")
         .doc(id)
         .delete()
+        .catch((error) => {
+            $('#deleteError').text("There was an issue communicating with the DB. Please try again later.");
+        })
         .then(() => {
             reloadEvents();
             $('#deleteModal').removeClass("is-active");
@@ -319,9 +362,15 @@ function loadEvents(ID) {
                 });
                 document.getElementById(ID).appendChild(addEventElem);
             }
-            let eventCard = "";
-            let element = response.docs;
-            element.forEach((e) => {
+
+            let docs = response.docs;
+            docs.sort((e) => {
+                return Date.parse(e.data().Date);
+            });
+            docs.forEach((e) => {
+                if (Date.parse(e.data().Date) < Date.now()) {
+                    return;
+                }
                 // create event
                 let eventElem = document.createElement("div");
                 $(eventElem).addClass("event");
